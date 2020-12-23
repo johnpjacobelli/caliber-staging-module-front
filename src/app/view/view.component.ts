40efeab9166c5baadd6368024a69c8ba1bd7578e
Young Ha Shin <YHS@Youngs-MBP.verizon.net>
2020-12-23 11:50:29 -0500

    ?

--- src/app/view/view.component.ts
+++ src/app/view/view.component.ts
@@ -1,3 +1,4 @@
+import { AssociateService } from './../services/associate.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AssociateService) { }
 
   ngOnInit(): void {
   }
