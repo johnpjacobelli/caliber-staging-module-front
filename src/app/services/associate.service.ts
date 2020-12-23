d5f8d2655f81aed582c7c11fcc5d547e8f944f87
Young Ha Shin <YHS@Youngs-MBP.verizon.net>
2020-12-23 16:23:31 -0500

    added obs

--- src/app/services/associate.service.ts
+++ src/app/services/associate.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Associate } from './../models/associate.model';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { URL } from './../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AssociateService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) {  }
+
+  getAllAssociates(id:number): Observable<Associate[]> { 
+    return this.http.post<Associate[]>(`${URL}/associates`, id)  }  
 }
