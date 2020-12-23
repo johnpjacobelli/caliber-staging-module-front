72e09288c3643be598e5a52b89da193017f90131
Matthew <mlafleur08@gmail.com>
2020-12-23 09:50:55 -0600

    create view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule
