d3983ac42bfb6cc69c5ad98c6c8a9e43cb905729
Young Ha Shin <YHS@Youngs-MBP.verizon.net>
2020-12-23 10:55:03 -0500

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AssociateComponent } from './associate/associate.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AssociateComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/associate/associate.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-associate',
+  templateUrl: './associate.component.html',
+  styleUrls: ['./associate.component.css']
+})
+export class AssociateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getAllAssociates(id){
+
+  }
+
+  getAllNewAssociates(){
+
+  }
+
+}
