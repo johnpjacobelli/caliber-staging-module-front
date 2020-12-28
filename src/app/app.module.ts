c6860268be5432ae10c5fe76249b7b0ca07f2439
aaknox <azhya.knox@gmail.com>
2020-12-28 14:22:15 -0600

    fixed filepath for swot component inside of app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ViewComponent } from './components/view/view.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SwotComponent } from './swot/swot.component';
+import { SwotComponent } from './components/swot/swot.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0}),
+        animate(2000, style({ opacity: 1}))
+      ])
+    ])
+  ]
 })
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
