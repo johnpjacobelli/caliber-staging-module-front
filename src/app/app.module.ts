ad1e1310925e6263ffea02966d0738543f9fe37a
aaknox <azhya.knox@gmail.com>
2020-12-28 14:52:40 -0600

    removed comment of non-existent associateComponent after talking to Team 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-
-// NOTE TO OTHER TEAMS: The below component no longer exist and has been commented
-// out to prevent compile errors. -Azhya Knox (T2)
-// import { AssociateComponent } from './components/associate/associate.component';
 import { ViewComponent } from './components/view/view.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  animations: [
-    trigger('fade', [
-      transition('void => *', [
-        style({ opacity: 0}),
-        animate(2000, style({ opacity: 1}))
-      ])
-    ])
-  ]
+  styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
