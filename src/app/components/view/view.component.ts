ea4eb753287546163dbc953275beb4e9b61d4241
aaknox <azhya.knox@gmail.com>
2020-12-28 12:24:33 -0600

    fixed path to associate model import in view.component.ts file

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -1,6 +1,6 @@
 import { AssociateService } from './../services/associate.service';
 import { Component, OnInit } from '@angular/core';
-import { Associate } from '../models/associate.model';
+import { Associate } from '../../models/associate-model/associate.model';
 
 @Component({
   selector: 'app-view',
