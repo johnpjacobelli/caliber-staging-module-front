8bbdfb0d4dc590a82b178b3e1184d7576f84c49f
Young Ha Shin <YHS@Youngs-MBP.verizon.net>
2020-12-28 14:10:39 -0500

    ?

--- src/app/services/associate.service.ts
+++ src/app/services/associate.service.ts
@@ -16,10 +16,7 @@ export class AssociateService {
   }
 
   getAllNewAssociates(id:number): Observable<Associate[]> { 
-    return this.http.get<Associate[]>(`${URL}/associates/new?manager={id}`)  
+    return this.http.get<Associate[]>(`${URL}/associates/new?manager=${id}`)  
   }
 }
-
-
-///associates/new?manager={id}
 
