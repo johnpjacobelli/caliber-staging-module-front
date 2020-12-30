992d7f224b41f61c639a6f6508d0c292ecf0cca4
Matthew <>
2020-12-30 16:40:33 -0600

    setup the sessionStorage for view-associate

--- src/app/components/view-associate/view-associate.component.ts
+++ src/app/components/view-associate/view-associate.component.ts
@@ -3,6 +3,7 @@ import { AssociateService } from '../../services/associate/associate.service';
 import { Associate } from '../../models/associate-model/associate.model';
 import { Component, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Component({
@@ -14,12 +15,22 @@ export class ViewAssociateComponent implements OnInit {
 
   associates: Associate[];
   newAssociates: Associate[];
+  private associateSubject: BehaviorSubject<Associate>;
+  public associate: Observable<Associate>; 
   testAssociate = new Associate(1, 'SF-1234', 'testEmail@email.com', 'test', 'tester', 14, 379, 'Training');
 
-  constructor(private service: AssociateService, private modalService: NgbModal) { }
+  constructor(private service: AssociateService, private modalService: NgbModal) { 
+    this.associateSubject = new BehaviorSubject<Associate>(JSON.parse(sessionStorage.getItem('currentUser')));  
+    this.associate = this.associateSubject.asObservable();
+  }
 
   ngOnInit(): void {
+    console.log(this.associateValue.email)
     this.getAllAssociates(1);
+  }
+
+  public get associateValue(): Associate {
+    return this.associateSubject.value;
   }
 
   open() {
