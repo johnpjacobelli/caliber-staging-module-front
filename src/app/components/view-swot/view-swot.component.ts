816691c304ab0a6507cca4dff10ca50b37a4de31
Metsuryuu <qjroman@gmail.com>
2020-12-31 17:55:49 -0500

    Fixed modal update and some code cleaning

--- src/app/components/view-swot/view-swot.component.ts
+++ src/app/components/view-swot/view-swot.component.ts
@@ -36,8 +36,7 @@ export class ViewSwotComponent implements OnInit {
   openUpdatePage(swotItem: SwotItem, swotAnalysisId: number){
     swotItem.swotAnalysisId = swotAnalysisId;
     const modalRef = this.modalService.open(UpdateItemComponent);
-    modalRef.componentInstance.name = 'UpdateSwot';    
-    //this.router.navigate(['/updateItem', JSON.stringify(swotItem)]);
+    modalRef.componentInstance.name = 'UpdateSwot';
   }
 
   open() {
