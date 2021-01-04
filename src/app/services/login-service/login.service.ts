c4a7e1213b641a69e52bf1bad4deb199265b7c7c
Metsuryuu <qjroman@gmail.com>
2021-01-04 10:24:31 -0500

    Pushing login service and client message model

---
+++ src/app/services/login-service/login.service.ts
@@ -0,0 +1,30 @@
+import { ClientMessage } from './../../models/client-message-model/client-message-model';
+import { BASE_URL, LOCAL_URL } from './../../../environments/environment.prod';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  public managerId:number =0;
+  constructor(private client:HttpClient) { }
+
+  //This method will retrieve the manger ID associated with the email address provided.
+storeManagerIdFromServer(email:string)
+{
+  console.log("In MangerID retriever! " + "Email is: " +email)
+  this.client.post<ClientMessage>(`${LOCAL_URL}/getmanager`,new ClientMessage(email)).subscribe(
+    data => {
+      this.managerId = parseInt(data.message)
+      console.log("Manager ID: " + this.managerId)
+    }
+
+
+  )
+}
+
+
+}
