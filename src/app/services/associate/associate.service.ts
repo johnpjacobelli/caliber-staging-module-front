e6ad54b466bdd87bbe54dd1ab52f313ddcc3a1b6
Ben <benjamin.johnston1998@gmail.com>
2021-01-05 11:00:34 -0500

    Fixed bug

--- src/app/services/associate/associate.service.ts
+++ src/app/services/associate/associate.service.ts
@@ -3,6 +3,7 @@ import { Associate } from '../../models/associate-model/associate.model';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { URL } from './../../../environments/environment.prod';
+import { LOCAL_URL } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +13,11 @@ export class AssociateService {
   constructor( private http: HttpClient ) {  }
 
   getAllAssociates(id:number): Observable<Associate[]> { 
-    return this.http.get<Associate[]>(`${URL}/associates?manager=${id}`)  
+    return this.http.get<Associate[]>(`${LOCAL_URL}/associates?manager=${id}`)  
   }
 
   getAllNewAssociates(id:number): Observable<Associate[]> { 
-    return this.http.get<Associate[]>(`${URL}/associates/new?manager=${id}`)  
+    return this.http.get<Associate[]>(`${LOCAL_URL}/associates/new?manager=${id}`)  
   }
 }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,7 @@ export const environment = {
 };
 
 
-export const LOCAL_URL = `http://localhost:8080/`;
+export const LOCAL_URL = `http://localhost:8081`;
 export const CALIBER_URL = `https://caliber2-mock.revaturelabs.com/mock`;
 export const BASE_URL = `http://localhost:8080/`;
 
