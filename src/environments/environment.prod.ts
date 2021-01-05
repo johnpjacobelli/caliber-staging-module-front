ba54cdf4551d6aa0aa5e24d47393359b4973fcc9
aaknox <azhya.knox@gmail.com>
2021-01-04 22:51:13 -0600

    beginning styling of view-swot-component, merging in Team 1's changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ export const environment = {
 
 };
 export const LOCAL_URL = `http://localhost:8080`;
-
 export const URL = `http://localhost:8080`;
 // export const URL = `http://ec2-54-237-27-253.compute-1.amazonaws.com:8080/`;
 export const CALIBER_URL = `https://caliber2-mock.revaturelabs.com/mock`;
