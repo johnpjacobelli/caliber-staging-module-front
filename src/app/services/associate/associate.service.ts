2b568b9de838d64030f917d69121dd9508959d30
Ben <benjamin.johnston1998@gmail.com>
2021-01-06 11:08:43 -0500

    Added HTTPHeaders to Login & Associate services

--- src/app/services/associate/associate.service.ts
+++ src/app/services/associate/associate.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Associate } from '../../models/associate-model/associate.model';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -10,13 +10,22 @@ import { environment } from 'src/environments/environment.prod';
 export class AssociateService {
 
   constructor( private http: HttpClient ) {  }
-
+  httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*/*',
+        'Access-Control-Allow-Methods': 'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type',
+      }
+    )
+  }
   getAllAssociates(id:number): Observable<Associate[]> { 
-    return this.http.get<Associate[]>(`${environment.BASE_URL}associates?manager=${id}`)  
+    return this.http.get<Associate[]>(`${environment.BASE_URL}associates?manager=${id}`,this.httpOptions)  
   }
 
   getAllNewAssociates(id:number): Observable<Associate[]> { 
-    return this.http.get<Associate[]>(`${environment.BASE_URL}associates/new?manager=${id}`)  
+    return this.http.get<Associate[]>(`${environment.BASE_URL}associates/new?manager=${id}`,this.httpOptions)  
   }
 }
 
--- src/app/services/login-service/login.service.ts
+++ src/app/services/login-service/login.service.ts
@@ -1,7 +1,7 @@
 import { environment } from 'src/environments/environment.prod';
 import { ClientMessage } from './../../models/client-message-model/client-message-model';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -11,12 +11,21 @@ export class LoginService {
 
   public managerId:number = 0;
   constructor(private client:HttpClient) { }
-
+  httpOptions = {
+    headers: new HttpHeaders(
+      {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*/*',
+        'Access-Control-Allow-Methods': 'OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token, content-type',
+      }
+    )
+  }
   //This method will retrieve the manger ID associated with the email address provided.
 storeManagerIdFromServer(email:string)
 {
   console.log("In MangerID retriever! " + "Email is: " +email)
-  this.client.post<ClientMessage>(`${environment.BASE_URL}getmanager`,new ClientMessage(email)).subscribe(
+  this.client.post<ClientMessage>(`${environment.BASE_URL}getmanager`,new ClientMessage(email),this.httpOptions).subscribe(
     data => {
       this.managerId = parseInt(data.message)
       sessionStorage.setItem('managerId', JSON.stringify(this.managerId));
