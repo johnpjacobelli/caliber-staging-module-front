6622067a78b25791a148b75a60dbfd7b801f7c9c
zoepiptall <56895489+zoepiptall@users.noreply.github.com>
2021-02-02 18:07:55 -0500

    WIP: added comment feild to nbModal

--- src/app/models/swot-model/swot-item.ts
+++ src/app/models/swot-model/swot-item.ts
@@ -3,21 +3,24 @@ export class SwotItem {
     id: number;
     content: string;
     type: string;
+    comment: string;
     swotAnalysisId: number;
 
 
 
-    constructor(id: number, content: string, type: string, swotAnalysisId?: number) {
+    constructor(id: number, content: string, type: string, comment: string, swotAnalysisId?: number) {
 
         if (swotAnalysisId) {
             this.id = id;
             this.content = content;
             this.swotAnalysisId = swotAnalysisId;
             this.type = type;
+            this.comment =comment;
         } else {
             this.id = id;
             this.content = content;
             this.type = type;
+            this.comment = comment;
         }
 
     }
