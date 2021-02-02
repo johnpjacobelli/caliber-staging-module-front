f5326109992766ce2d67af5c01892711180a3ffb
john <laojohnmatthew@gmail.com>
2021-02-02 09:49:43 -0800

    add name field in the creating swot feature

--- src/app/models/swot-model/swot-item.ts
+++ src/app/models/swot-model/swot-item.ts
@@ -4,20 +4,23 @@ export class SwotItem {
     content: string;
     type: string;
     swotAnalysisId: number;
+    swotName: string;
 
 
 
-    constructor(id: number, content: string, type: string, swotAnalysisId?: number) {
+    constructor(id: number, content: string, type: string, swotName: string, swotAnalysisId?: number) {
 
         if (swotAnalysisId) {
             this.id = id;
             this.content = content;
             this.swotAnalysisId = swotAnalysisId;
             this.type = type;
+            this.swotName = swotName
         } else {
             this.id = id;
             this.content = content;
             this.type = type;
+            this.swotName = swotName;
         }
 
     }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,8 +12,7 @@ export const environment = {
     "measurementId": "G-DP6XDH9DTW"
   },
   CALIBER_URL: `https://caliber2-mock.revaturelabs.com/mock`,
-  BASE_URL: `http://localhost:8081/`
-  //  BASE_URL: `http://ec2-54-237-27-253.compute-1.amazonaws.com:8081/`
+  BASE_URL: `http://ec2-54-237-27-253.compute-1.amazonaws.com:8081/`
 };
 
 
