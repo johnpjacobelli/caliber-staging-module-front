38d78a3e27e46552f319a5f093a25c44f8f29f74
unknown <adam.alouani@gmail.com>
2021-02-03 22:09:01 +0100

    add a swot type update

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -14,7 +14,7 @@ export class AddItemComponent implements OnInit {
 
   swotItem : SwotItem;
   @Input() parentSwot : Swot;
-  
+
   constructor(private swotService : SwotService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
