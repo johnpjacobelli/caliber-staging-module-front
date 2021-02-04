21b6284b80933a19971507bd12bb95e32a3812a4
mdimgba <59780947+mdimgba@users.noreply.github.com>
2021-02-04 16:33:08 -0600

    Working on implementing toast message. Getting propper message to display for each button, and auto-fade after 10s

--- src/app/components/toast-message/toast-message.component.ts
+++ src/app/components/toast-message/toast-message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-toast-message',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toast-message.component.css']
 })
 export class ToastMessageComponent implements OnInit {
-
+  message: String;
+  buttonId: String;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /*
+  Function takes in information for which submit button was pressed create swot, update swot, or delete swot
+  Function then changes the message variable to describe the action taken by the user
+  */
+  setToastMessage(actionDetails: any): void {
+    // new swot message
+    if(actionDetails == "create") {
+    this.message = "Success! New SWOT item has been added.";
+    this.buttonId = '#createButton';
+    this.setTimeVisible();
+    }
+    //update message
+    else if(actionDetails == "update") {
+    this.message = "Success! SWOT item has been updated.";
+    this.buttonId = '#addButton';
+    }
+    // delete message -> see delete method in view-swot component
+    else if(actionDetails == "delete") {
+    this.message =  "";
+    this.buttonId = 'deleteButton';
+    }
+  }
+
+  /*
+    Function used to set the amount of time toasts are visible before disappearing
+    Also to determine how many toasts can appear on screen at once?
+  */
+  setTimeVisible(): void {
+    //$(document).ready(function(){
+      //$(this.buttonId).click(function(){
+        $('.toast').toast({delay: 10000});
+        $('.toast').toast('show');
+      //});
+   // });
   }
 
 }
